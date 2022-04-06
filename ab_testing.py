62cc8b8117a6a76bc71a161472d2b6b4b942c2b5
Cevahir ÖZGÜR <89588465+cvhr@users.noreply.github.com>
2022-04-06 23:27:26 +0300

    Update ab_testing.py

--- ab_testing.py
+++ ab_testing.py
@@ -1,36 +1,3 @@
-########################  AB Testi ile Bidding Yöntemlerinin Dönüşümünün Karşılaştırılması  #####################
-#İş Problemi
-# Facebook kısa süre önce mevcut "maximumbidding" adı verilen teklif verme türüne alternatif olarak
-# yeni bir teklif türü olan "average bidding"’i tanıttı.
-
-#Müşterilerimizden biri olan bombabomba.com, bu yeni özelliği test etmeye karar verdi
-# ve average bidding'in maximumbidding'den daha fazla dönüşüm getirip getirmediğini
-# anlamak için bir A/B testi yapmak istiyor.
-
-# A/B testi 1 aydır devam ediyor ve bombabomba.com şimdi sizden bu A/B testinin sonuçlarını analiz etmenizi bekliyor.
-# Bombabomba.com için nihai başarı ölçütü Purchase'dır. Bu nedenle, istatistiksel testler için
-# Purchase metriğine odaklanılmalıdır.
-
-#Veri Seti Hikayesi
-#Bir firmanın web site bilgilerini içeren bu veri setinde kullanıcıların gördükleri ve tıkladıkları reklam sayıları
-# gibi bilgilerin yanı sıra buradan gelen kazanç bilgileri yer almaktadır.
-# Kontrol ve Test grubu olmak üzere iki ayrı veri seti vardır.
-# Bu veri setleri ab_testing.xlsx excel’inin ayrı sayfalarında yer almaktadır.
-# Kontrol grubuna Maximum Bidding, test grubuna Average Bidding uygulanmıştır.
-
-# Impression Reklam görüntüleme sayısı
-# Click: Görüntülenen reklama tıklama sayısı
-# Purchase: Tıklanan reklamlar sonrası satın alınan ürün sayısı
-# Earning: Satın alınan ürünler sonrası elde edilen kazanç
-
-#################################### Görev 1: Veriyi Hazırlama ve Analiz Etme ####################################
-
-#Adım 1: ab_testing_data.xlsx adlı kontrol ve test grubu verilerinden oluşan veri setini okutunuz.
-#Kontrol ve test grubu verilerini ayrı değişkenlere atayınız.
-
-# import os
-# print("Current working directory: {0}".format(os.getcwd()))
-# os.chdir("D:\yazilim/VBO")
 
 import itertools
 import pandas as pd
